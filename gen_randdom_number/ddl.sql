0bd2facec7b69bc492c0cb95f66ec4b19360e615
mingyuchoo <mingyuchoo@gmail.com>
2023-02-18 05:46:16 +0900

    changed structures

--- gen_randdom_number/ddl.sql
+++
@@ -1,7 +0,0 @@
--- ---------------------------------------------------
--- table
--- ---------------------------------------------------
-CREATE TABLE `temp` (
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `temper` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
