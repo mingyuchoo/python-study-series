8223b85c32ef501a213a09f958660a661f10ef3e
my-name <my-email@address.com>
2024-11-01 08:58:36 +0900

    updated

--- langchain/tutorials/chatbot.py
+++ langchain/tutorials/chatbot.py
@@ -1,9 +1,10 @@
 from langchain_aws import ChatBedrock
+from langgraph.utils.config import var_child_runnable_config
 
 model = ChatBedrock(model_id="anthropic.claude-3-5-sonnet-20240620-v1:0")
 
 # #####################################
-# 1.
+# 
 # #####################################
 from langchain_core.messages import HumanMessage
 
@@ -17,7 +18,7 @@ print(result.content)
 
 
 # #####################################
-# 2.
+# 
 # #####################################
 
 from langchain_core.messages import AIMessage
@@ -33,7 +34,7 @@ result = model.invoke(
 print(result.content)
 
 # #####################################
-# 3. Message persistence
+# Message persistence
 # #####################################
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
@@ -70,18 +71,109 @@ output["messages"][-1].pretty_print()
 
 
 # #####################################
-# 4.Prompt templates
+# Prompt templates
+# #####################################
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder 
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You talk like a pirate. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+workflow =StateGraph(state_schema=MessagesState)
+
+def call_model(state: MessagesState):
+    chain = prompt | model
+    response = chain.invoke(state)
+    return {"messages": response}
+
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+
+config = {"configurable": {"thread_id": "abc345"}}
+query = "Hi! I'm Jim."
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+
+
+query = "What is my name?"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}."
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+from typing import Sequence
+
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages 
+from typing_extensions import Annotated, TypedDict
+
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
+
+workflow = StateGraph(state_schema=State)
+
+
+def call_model(state: State):
+    chain = prompt | model
+    response = chain.invoke(state)
+    return {"messages": [response]}
+
+
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "abc456"}}
+
+query = "Hi! I'm Bob."
+language = "Spanish"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke(
+    {"messages": input_messages, "language": language},
+    config,
+)
+output["messages"][-1].pretty_print()
+
+query = "What is my name?"
+
+input_messages = [HumanMessage(query)]
+output = app.invoke(
+    {"messages": input_messages},
+    config,
+)
+output["messages"][-1].pretty_print()
+# #####################################
+# Managing Conversation History
 # #####################################
 
 # #####################################
-# 5.
-# #####################################
-
-# #####################################
-# 6.
-# #####################################
-
-# #####################################
-# 7.
+# Streaming
 # #####################################
  
