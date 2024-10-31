5a1c38b91745eb6d8699349fd0a5454dd694a280
mingyuchoo <mingyuchoo@gmail.com>
2024-10-31 12:47:37 +0900

    added

--- langchain/tutorials/script.py
+++ langchain/tutorials/script.py
@@ -2,6 +2,10 @@ import getpass
 import os
 
 
+# #####################################
+# Using Language Models
+# #####################################
+
 from langchain_aws import ChatBedrock
 
 model = ChatBedrock(model_id="anthropic.claude-3-5-sonnet-20240620-v1:0")
@@ -19,16 +23,31 @@ from langchain_core.output_parsers import StrOutputParser
 parser = StrOutputParser()
 
 chain = model | parser
-chain.invoke(messages)
 
-from langchain_core.prompt import ChatPromptTemplate
+result = chain.invoke(messages)
+print(result)
+
+
+# #####################################
+# Prompt Templates
+# #####################################
+
+from langchain_core.prompts import ChatPromptTemplate
 
 system_template = "Translate the following into {language}:"
 prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{text}")]
 )
 
+result = prompt_template.invoke({"language": "italian", "text": "hi"})
+print(result.to_messages())
+
+# #####################################
+# Chaining together components with LCEL
+# #####################################
+
 chain = prompt_template | model | parser
 
-result = chain.invoke({"langauge": "italian", "text": "hi"})
+result = chain.invoke({"language": "italian", "text": "hi"})
 print(result)
+
